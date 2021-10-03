9bb383ff8b6cae931ae4f830f7a0201a19bd10d0
GeorgeCouch <george.couch.14@gmail.com>
2021-10-03 18:26:13 -0400

    Final

--- GeorgeCouchMultiThreading/GeorgeCouchMultiThreading/Source.cpp
+++ GeorgeCouchMultiThreading/GeorgeCouchMultiThreading/Source.cpp
@@ -1,3 +1,8 @@
+/* Block Comment Header
+Name George Couch and Brett Smitch
+Date 10/03/2021
+Our multithreading assignment.
+*/
 #include <iostream>
 #include <thread>
 #include <mutex>
@@ -11,10 +16,12 @@ int abortAllLaunches = 0;
 
 void abortLaunch()
 {
-    // George: Buffer between enter key so that the last key press doesn't stretch into this one
+    // Buffer between enter key so that the last key press doesn't stretch into this one
     this_thread::sleep_for(chrono::seconds(1));
+    // Thread dedicated to listening for ENTER key
     while (abortAllLaunches == 0)
     {
+        // Set abort to true if enter is pressed
         if (GetKeyState(VK_RETURN) & 0x8000)
         {
             abortAllLaunches = 1;
@@ -24,11 +31,14 @@ void abortLaunch()
 
 void Launch(string rocketNum)
 {
+    // Ensures only one thread executes this block of code at a time
         Locker.lock();
+        // Executes if abortALL Launches != 1. In other words, makes check to see if enter was pressed
         if (abortAllLaunches == 0)
         {
+            // Get id for current thread
             thread::id currentThread = this_thread::get_id();
-            // George: Threads can arrive in wrong order, keeping this just for formatting from picture
+            // Threads can arrive in wrong order, keeping this just for formatting from picture
             if (rocketNum == "#1")
             {
                 cout << "Thread ID: " << currentThread << " has been spawned to launch rockets!\n\n" << endl;
@@ -39,32 +49,39 @@ void Launch(string rocketNum)
                 cout << "\tPress Enter at Space Base to stop the Space Cadet launching rockets from this spawn thread at the Rocket Pad....\n\n" << endl;
             }
 
+            // Thread sleeps for 3 seconds
             this_thread::sleep_for(chrono::seconds(3));
-            // George: Second check so that I can abort a launch mid initiation
+            // Second check so that I can abort a launch mid initiation
             if (abortAllLaunches == 0)
             {
                 cout << "*** The Space Cadet at the Rocket Pad is using the Spawn thread for Launching Local Rocket " << rocketNum << " from spawned thread.\n" << endl;
                 cout << "Getting the current spawned Thread ID:" << currentThread << "\n" << endl;
             }
         }
+        // Allow for other threads
         Locker.unlock();
 }
 
 int main()
 {
+    // Get main thread id
 	thread::id mainThread = this_thread::get_id();
 	cout << "\tWelcome to Rocket Launcher!\n" << endl;
 	cout << "Getting the Main Thread ID:" << mainThread << endl;
 	cout << "Press enter to start launching rockets locally from Space base using main thread!" << endl;
-	cin.ignore(); // George: This can cause a bug where I have to press enter twice here, so far fixed by saving program and running again (CTRL + S)
+    // Press enter to continue
+	cin.ignore();
 	cout << "Starting to launch rockets on the Rocket Pad controlling the Launches from main thread at the Main Space Base!" << endl;
-	this_thread::sleep_for(chrono::seconds(3)); //George: can possibly change this to Dragon Fight format so looks better with ...
+    // Thread sleeps for 3 seconds
+	this_thread::sleep_for(chrono::seconds(3));
 	cout << "\n\n*** Space Base using main thread is launching local Rocket #1 from Main thread at Space Base.\n" << endl;
 	cout << "Space Base's Main thread is Launching Rocket #1 from Main thread at Space Base." << endl;
 	cout << "Press enter to transfer control to the Space Cadet at the Space Pad to start launching rockets from spawn thread!" << endl;
+    // Press enter to continue
 	cin.ignore();
 	cout << "Getting Main Thread ID:" << mainThread << endl;
 	cout << "Press enter to stop reclaim rocket launch control from the Space Cadet at the Rocket Pad to Space Base main thread only!\n" << endl;
+    // Create rocket threads
 	thread rocket1{ Launch,"#1" };
 	thread rocket2{ Launch,"#2" };
 	thread rocket3{ Launch,"#3" };
@@ -75,58 +92,71 @@ int main()
 	thread rocket8{ Launch,"#8" };
 	thread rocket9{ Launch,"#9" };
 	thread rocket10{ Launch,"#10" };
+
+    // Create abort/enter key listener
     thread abort{ abortLaunch };
 
+    // Main thread waits to join with other thread
     if (rocket1.joinable())
     {
         rocket1.join();
     }
 
+    // Main thread waits to join with other thread
     if (rocket2.joinable())
     {
         rocket2.join();
     }
 
+    // Main thread waits to join with other thread
     if (rocket3.joinable())
     {
         rocket3.join();
     }
 
+    // Main thread waits to join with other thread
     if (rocket4.joinable())
     {
         rocket4.join();
     }
 
+    // Main thread waits to join with other thread
     if (rocket5.joinable())
     {
         rocket5.join();
     }
 
+    // Main thread waits to join with other thread
     if (rocket6.joinable())
     {
         rocket6.join();
     }
 
+    // Main thread waits to join with other thread
     if (rocket7.joinable())
     {
         rocket7.join();
     }
 
+    // Main thread waits to join with other thread
     if (rocket8.joinable())
     {
         rocket8.join();
     }
 
+    // Main thread waits to join with other thread
     if (rocket9.joinable())
     {
         rocket9.join();
     }
 
+    // Main thread waits to join with other thread
     if (rocket10.joinable())
     {
         rocket10.join();
     }
 
+    // Occurs if Enter was pressed to abort
     if (abortAllLaunches == 1)
     {
         cout << "You Pressed enter! Space Base using Main Thread to reclaimed rocket launch control." << endl;
@@ -137,17 +167,17 @@ int main()
     cout << "Checking Main Thread ID:" << mainThread << endl;
     cout << "About to Join thread, AKA wait for Thread" << endl;
     cout << "Getting Main Thread ID:" << mainThread << endl;
-    // George: Buffer for potential formatting and to not let last enter press stretch into the next one
+    // Buffer for formatting and to not let last enter press stretch into the next one
     this_thread::sleep_for(chrono::seconds(1));
     cout << "Thread has been joined!" << endl;
     cout << "Getting Main Thread ID:" << mainThread << endl;
     cout << "Press Enter to Exit Program" << endl;
-    // George: For some reason a normal cin.ignore wouldn't work here, this is more effective, can also activate both listeners at the same time
+    // For some reason a normal cin.ignore wouldn't work here, this is more effective, can also activate both listeners at the same time
     while (!(GetKeyState(VK_RETURN) & 0x8000))
     {
     }
 
-    // George: Placed abort.join down here so that it's loop will end for sure given that you must press enter to get here, could also just call exit() and not join
+    // Join abort thread
     if (abort.joinable())
     {
         abort.join();
